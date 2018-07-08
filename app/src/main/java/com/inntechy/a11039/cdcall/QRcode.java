b31eb70e8e1f32e06ad2e0eefb8f9e01507f92f0
ye15331374 <595199759@qq.com>
2018-07-08 20:50:42 +0800

    文字转二维码

---
+++ app/src/main/java/com/inntechy/a11039/cdcall/QRcode.java
@@ -0,0 +1,36 @@
+package com.inntechy.a11039.cdcall;
+
+import android.graphics.Bitmap;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by yinh2 on 2018/1/8.
+ */
+
+public class QRcode {
+    public static Bitmap encodeAsBitmap(String str){
+        Bitmap bitmap = null;
+        BitMatrix result = null;
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            result = multiFormatWriter.encode(new String(str.getBytes("UTF-8"),"ISO-8859-1"), BarcodeFormat.QR_CODE, 700, 700);
+            // 使用 ZXing Android Embedded 要写的代码
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            bitmap = barcodeEncoder.createBitmap(result);
+        } catch (WriterException e){
+            e.printStackTrace();
+        } catch (IllegalArgumentException iae){ // ?
+            return null;
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+}
